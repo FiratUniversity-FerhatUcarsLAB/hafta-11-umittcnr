d53175a4d62f2bc54f3559bbef8bcc1dafd23624
ümitcan <cinarx04@gmail.com>
2025-11-30 19:39:52 +0300

    Implement date printing methods in Exercise4_1

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,12 @@
 public class Exercise4_1 {
-
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
-    }
-
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
-    }
-
     public static void main(String[] args) {
-        // TODO: test methods
+       printAmerican("pazartesi ", 22 , " haziran ", 2025);
+       printEuropan(22 , " haziran ", 2025 , " pazartesi ");
+    }
+    public static void printAmerican(String gun,int tarih,String ay,int yil){
+        System.out.println(gun + tarih + ay + yil);
+    }
+    public static void printEuropan(int tarih,String ay,int yil,String gun){
+        System.out.println(tarih + ay + yil + gun);
     }
 }
