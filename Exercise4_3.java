9b3b607e60717f34b27fe5992ef917af2d02cd18
ümitcan <cinarx04@gmail.com>
2025-12-01 09:52:55 +0300

    Exercise4_3 Stack diyagramı olluşturma görevi TAMAMLANDI
    Refactor Exercise4_3 class to include comments and improve structure.

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,24 +1,55 @@
-public class Exercise4_3 {
+/*
+ * Ad Soyad: [ümitcan çinar]
+ * Ogrenci No: [250541009]
+ * Tarih: [1/12/2025]
+ * Aciklama: Gorev 3 - Stack diyagramı olluşturma görevi
+ *
+ * Bu program stack diyagramları sayesinde paramtre aergüman yapısını ve sırasını daha iyi kavramamızı sağlar.
+ */
 
-    public static void zoop() {
-        baffle();
-        System.out.print("You wugga ");
-        baffle();
-    }
+public class Exercise4_1 {
 
-    public static void main(String[] args) {
-        System.out.print("No, I ");
-        zoop();
-        System.out.print("I ");
-        baffle();
-    }
 
-    public static void baffle() {
-        System.out.print("wug");
-        ping();
-    }
+        public static void zoop() {
+            baffle();
+            System.out.print("You wugga");
+            baffle();
+        }
 
-    public static void ping() {
-        System.out.println(".");
-    }
+        public static void main(String[] args) {
+            System.out.print("No, I ");
+            zoop();
+            System.out.print(" I ");
+            baffle();
+        }
+
+        public static void baffle() {
+            System.out.print("wug");
+            ping();
+        }
+
+        public static void ping() {
+            System.out.println(".");
+        }
+    
 }
+ /*
+ main go to zoop (firstly print "No.I")
+ zoop go to baffle (firstly print "wug")
+ baffle go to ping
+ ping print (".")
+ and come back zoop , it print ("You wugga")
+ zoop go to baffle
+ baffle print ("wug") and go to ping
+ ping print (".")
+ and come back to main it print ("I")
+ main go to baffle and it print ("wug")
+ and baffle go to ping and it print (".")
+  */
+
+/* tam çıktı
+No, I wug.
+you wuggawug.
+ I wug.
+ */
+
